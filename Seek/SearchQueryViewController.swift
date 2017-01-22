7763737fdd3cec104d4f64b556e778877822159e
Wilson Ding <shodai100@gmail.com>
2017-01-22 07:58:30 -0500

    Final push before demo

--- Seek/SearchQueryViewController.swift
+++ Seek/SearchQueryViewController.swift
@@ -17,9 +17,15 @@ class SearchQueryViewController: UIViewController, YTPlayerViewDelegate, UITextF
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var seekButton: UIButton!
     @IBOutlet weak var timestampButton: UIButton!
+    @IBOutlet weak var loadAudio: UILabel!
+    @IBOutlet weak var loadVideo: UILabel!
+    @IBOutlet weak var loadFinal: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     
     var selectedUrl = ""
-    public var selectedTime : Float = 0.0
+    var selectedTime : Float = 0.0
+    
+    var currentPercentage: Float = 0.0
     
     var timestampArray : [Timestamp] = []
     
@@ -44,15 +50,51 @@ class SearchQueryViewController: UIViewController, YTPlayerViewDelegate, UITextF
         
         self.playerView.cueVideo(byId: selectedUrl, startSeconds: selectedTime, suggestedQuality: .default)
         
-        timestampArray.append(Timestamp(timestamp: 0.0, detail: "Return To Beginning"))
+        loadAudio.text = "Loading Audio Analysis..."
+        loadVideo.text = "Loading Video Analysis..."
+        loadFinal.text = "Calculating Timestamps..."
+        progressView.isHidden = true
+        loadAudio.isHidden = true
+        loadVideo.isHidden = true
+        loadFinal.isHidden = true
+        timestampButton.isHidden = true
+        searchTextField.text = ""
+        timestampArray = []
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        currentPercentage = 0.0
+        update(new: currentPercentage)
+        
+        loadAudio.text = "Loading Audio Analysis..."
+        loadVideo.text = "Loading Video Analysis..."
+        loadFinal.text = "Calculating Timestamps..."
+        loadAudio.textColor = UIColor.black
+        loadVideo.textColor = UIColor.black
+        loadFinal.textColor = UIColor.black
+        progressView.isHidden = true
+        loadAudio.isHidden = true
+        loadVideo.isHidden = true
+        loadFinal.isHidden = true
+        timestampButton.isHidden = true
+        searchTextField.text = ""
+        timestampArray = []
+        
+        timestampArray.append(Timestamp(timestamp: 0.0, detail: "Return To Beginning"))
+        
         if selectedTime != 0.0 {
             self.playerView.cueVideo(byId: selectedUrl, startSeconds: selectedTime, suggestedQuality: .default)
             self.playerView.playVideo()
+        }
+    }
+    
+    func update(new: Float) {
+        if (new >= 1) {
+            progressView.setProgress(1, animated: true)
+        } else {
+            progressView.setProgress(new, animated: true)
         }
     }
     
@@ -78,6 +120,19 @@ class SearchQueryViewController: UIViewController, YTPlayerViewDelegate, UITextF
     }
     
     func sendDataToServerWith(id: String, query: String, endpoint: String) {
+        
+        self.progressView.isHidden = false
+        
+        if endpoint == "audiosearch" {
+            self.loadAudio.isHidden = false
+        }
+        
+        if endpoint == "videosearch" {
+            self.loadVideo.isHidden = false
+        }
+        
+        self.loadFinal.isHidden = false
+        
         Alamofire.request("https://f1882f9f.ngrok.io/\(endpoint)?v=\(id)&q=\(query)").responseJSON { response in
             print(response.response!) // HTTP URL response
             
@@ -96,9 +151,49 @@ class SearchQueryViewController: UIViewController, YTPlayerViewDelegate, UITextF
                 }
                 
                 self.timestampArray += jsonArray
+                
+                if endpoint == "audiosearch" {
+                    self.loadAudio.textColor = UIColor.green
+                    self.loadAudio.text = "Done!"
+                    self.currentPercentage = self.currentPercentage + 0.33
+                    self.update(new: self.currentPercentage)
+                }
+                
+                if endpoint == "videosearch" {
+                    self.loadVideo.textColor = UIColor.green
+                    self.loadVideo.text = "Done!"
+                    
+                    self.currentPercentage = self.currentPercentage + 0.33
+                    self.update(new: self.currentPercentage)
+                }
 
             } else {
                 print("JSON serialization failed. Raw data: \(response.result)")
+                
+                if endpoint == "audiosearch" {
+                    self.loadAudio.textColor = UIColor.red
+                    self.loadAudio.text = "Failed! API Key Max Usage Reached."
+                    self.currentPercentage = self.currentPercentage + 0.33
+                    self.update(new: self.currentPercentage)
+                }
+                
+                if endpoint == "videosearch" {
+                    self.loadVideo.textColor = UIColor.red
+                    self.loadVideo.text = "Failed! API Key Max Usage Reached."
+                    
+                    self.currentPercentage = self.currentPercentage + 0.33
+                    self.update(new: self.currentPercentage)
+                }
+            }
+            
+            if self.currentPercentage == 0.66 {
+                self.loadFinal.textColor = UIColor.green
+                self.loadFinal.text = "Done!"
+                
+                self.currentPercentage = self.currentPercentage + 0.34
+                self.update(new: self.currentPercentage)
+                
+                self.timestampButton.isHidden = false
             }
         }
     }
@@ -108,7 +203,6 @@ class SearchQueryViewController: UIViewController, YTPlayerViewDelegate, UITextF
             let youtubeId = selectedUrl.replacingOccurrences(of: "https://www.youtube.com/watch?v=", with: "")
             sendDataToServerWith(id: youtubeId, query: searchTextField.text!, endpoint: "audiosearch")
             sendDataToServerWith(id: youtubeId, query: searchTextField.text!, endpoint: "videosearch")
-            timestampButton.isHidden = false
         } else {
             createAlert(title:"Error", message: "The Search Query is not valid!")
         }
